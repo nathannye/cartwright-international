daec8fb252cfe726dbcfe4f23222288da01e5372
nathannye <79227199+nathannye@users.noreply.github.com>
2022-05-30 16:57:26 -0400

    new navbar and footer files with correct names

---
+++ components/Footer.js
@@ -0,0 +1,45 @@
+import Link from "next/link";
+import { EmailForm } from "../components/EmailForm";
+
+const Footer = ({ contact }) => {
+  return (
+    <footer>
+      <div className="topSplit">
+        <h1>Let’s make it happen, drop us a line</h1>
+        <EmailForm />
+      </div>
+      <div className="bottomSplit">
+        <div>
+          <Link href="/">
+            <a className="secondaryLink">Privacy Policy</a>
+          </Link>
+          <p> © {new Date().getFullYear()} Cartwright International</p>
+          <a
+            className="secondaryLink"
+            href="https://www.nye.design"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Made by Nathan Nye
+          </a>
+        </div>
+        <div>
+          {/* <a href={`mailto:${contact.data.email}`}>{contact.data.email}</a> •
+          <a href={`tel:${contact.data["phone-number"]}`}></a> */}
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+// export async function getStaticProps({ previewData }) {
+//   const client = createClient({ previewData });
+
+//   const contact = await client.getSingle("contact-us");
+
+//   return {
+//     props: { contact },
+//   };
+// }
+
+export default Footer;
