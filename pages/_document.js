3bc63bf37edd9429c01b9176d978533cabae1852
nathannye <79227199+nathannye@users.noreply.github.com>
2022-06-06 15:17:25 -0400

    new font loading

--- pages/_document.js
+++ pages/_document.js
@@ -28,7 +28,11 @@ function Document() {
           }}
         />{" "}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,300;0,400;0,500;1,300;1,400;1,500&family=Roboto+Flex:wght@100..800;&display=swap" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,300;0,400;0,500;1,300;1,400;1,500&family=Roboto:wght@100;300;400;500;700&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <body>
         <Main />
